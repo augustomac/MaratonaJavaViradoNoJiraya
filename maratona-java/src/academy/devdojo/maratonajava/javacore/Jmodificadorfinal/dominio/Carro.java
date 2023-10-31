42da0ee4f6d71e3b6ec8196bd5fe302dfa6dd5a8
Augusto Machado <augustoludovice@gmail.com>
2023-10-31 18:40:35 -0300

    Aula 77 - Tipo primitivo

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Carro.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
+
+public class Carro {
+    private String nome;
+    public static final double VELOCIDADE_LIMITE = 250; //constante sempre em MAIUSCULO e separado por _ para palavras compostas
+}
+
