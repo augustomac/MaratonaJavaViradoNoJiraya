10d9dff565e1cf87e82db09abdf62aea8c607863
Augusto Machado <augustoludovice@gmail.com>
2023-11-05 21:13:31 -0300

    Aula 85 - Classes Abstratas - 02

--- maratona-java/src/academy/devdojo/maratonajava/javacore/lclassesabstratas/test/FuncionarioTeste01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/lclassesabstratas/test/FuncionarioTeste01.java
@@ -1,12 +1,13 @@
 package academy.devdojo.maratonajava.javacore.lclassesabstratas.test;
-
-import academy.devdojo.maratonajava.javacore.lclassesabstratas.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.lclassesabstratas.dominio.Desenvolvedor;
 import academy.devdojo.maratonajava.javacore.lclassesabstratas.dominio.Gerente;
 
 public class FuncionarioTeste01 {
     public static void main(String[] args) {
         Gerente gerente = new Gerente("Luffy", 10000);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Usopp", 7000);
         System.out.println(gerente);
+        System.out.println(desenvolvedor);
 
     }
 
