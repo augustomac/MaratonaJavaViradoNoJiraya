08bd3dc00e28fa14417c3d625c5144dbd35df247
Augusto Machado <augustoludovice@gmail.com>
2023-11-05 22:07:32 -0300

    Aula 87 - Interfaces - Parte 01

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Minterfaces.dominio;
+
+//interface e meio que um contrato
+//em interface por padrão todos os métodos são publicos e abstratos
+public interface DataLoader {
+//    (public abstract) void load();
+// como escrevi acima já é sob entendido que o método é public absstract, podendo ser chmado como abaixo
+    void load();
+
+}
